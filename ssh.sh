6b2c51b854015faf08c3892c954456a08198ef4a
1320014053 <59201143+1320014053@users.noreply.github.com>
2022-09-05 03:08:26 +1200

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,10 @@
+wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -qq -n ngrok-stable-linux-amd64.zip
+apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
+echo root:asas | chpasswd
+mkdir -p /var/run/sshd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
+echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+/usr/sbin/sshd -D &
